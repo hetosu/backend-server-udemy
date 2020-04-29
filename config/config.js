1621eed068e7cb5c2d6749347a3e913762d8a57c
hetosu <hector.tobio@gmail.com>
2020-04-29 14:33:25 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports.SEED = '@este-es-@-un-seed-difficult';
 
 // Google
-module.exports.CLIENT_ID = '716004520200-kk106sht93u2jc1eaj7hcktafqcs8ec7.apps.googleusercontent.com';
+module.exports.CLIENT_ID = '*****************************************************';
